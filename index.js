0a3f4c2ff5d136cee1181ee5bae10d8f96bf2597
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2023-05-24 23:31:52 +0600

    Load Data From Server To Client Done

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+// Dotenv Er Jonne Lagbei.
 require("dotenv").config();
 
 // Use Middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://Doctors-Portal:gxEQ8qDL85jSHeeB@doctors-cluster-by-shad.jqobsrg.mongodb.net/`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-cluster-by-shad.jqobsrg.mongodb.net/`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,16 +22,19 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
+    const serviceCollection = client
+      .db("Doctors_Portal")
+      .collection("Services");
 
-   //  await client.close();
+    app.get("/Services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    //  await client.close();
   }
 }
 run().catch(console.dir);
